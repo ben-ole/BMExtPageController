43b2ae9e77edd8e3796b77dd8f0384e2f4918d8f
shakemno <info@shakemno.de>
2013-05-13 16:37:54 +0200

    added BMVerticalFlipTransition, updated NSLayoutConstraint+PlacementHelper accordingly

--- BMExtendablePageController/NSLayoutConstraint+PlacementHelper.m
+++ BMExtendablePageController/NSLayoutConstraint+PlacementHelper.m
@@ -28,8 +28,8 @@
     
 }
 
-+(void)stickView:(VIEW *)aView nextToSibling:(VIEW *)sibling direction:(BM_LAYOUT_DIRECTION)direction{
-    
++(void)stickView:(VIEW *)aView nextToSibling:(VIEW *)sibling direction:(BM_LAYOUT_DIRECTION)direction
+{    
     if(!aView || !aView.superview || !sibling) return;
     
     aView.translatesAutoresizingMaskIntoConstraints = FALSE;
@@ -37,35 +37,48 @@
     
     [NSLayoutConstraint removeConstraintsFromSuperView:aView];
     
-    if(direction == BM_LAYOUT_DIRECTION_RIGHT){
+    if(direction == BM_LAYOUT_DIRECTION_RIGHT)
+    {
         [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeRight multiplier:1.0 constant:0.]];
-        
-    }else if(direction  == BM_LAYOUT_DIRECTION_LEFT){
+    }
+    else if(direction == BM_LAYOUT_DIRECTION_LEFT)
+    {
         [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0.]];
     }
+    else if(direction == BM_LAYOUT_DIRECTION_TOP)
+    {
+        [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.]];
+    }
+    else if(direction == BM_LAYOUT_DIRECTION_BOTTOM)
+    {
+        [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.]];
+    }
     
-    [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.]];
-
-    [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.]];
-
     [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeWidth multiplier:1 constant:0.]];
-
     [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:sibling attribute:NSLayoutAttributeHeight multiplier:1 constant:0.]];
     
 }
 
-+(NSLayoutConstraint*)fillSuperView:(VIEW *)aView{
-    
++(NSLayoutConstraint*)fillSuperView:(VIEW *)aView
+{
+    return [NSLayoutConstraint fillSuperView:aView center:BM_LAYOUT_CENTER_X];
+}
+
++(NSLayoutConstraint*)fillSuperView:(VIEW*)aView center:(BM_LAYOUT_CENTER)center;
+{    
     if(!aView || !aView.superview) return nil;
     
     aView.translatesAutoresizingMaskIntoConstraints = FALSE;
     [NSLayoutConstraint removeConstraintsFromSuperView:aView];
     
-    NSLayoutConstraint* centerLayout = [NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:aView.superview attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.];
+    NSLayoutAttribute centerAttr = (BM_LAYOUT_CENTER_X == center) ? NSLayoutAttributeCenterX : NSLayoutAttributeCenterY;
+    NSLayoutAttribute posAttr = (BM_LAYOUT_CENTER_X == center) ? NSLayoutAttributeCenterY : NSLayoutAttributeCenterX;
+    
+    NSLayoutConstraint* centerLayout = [NSLayoutConstraint constraintWithItem:aView attribute:centerAttr relatedBy:NSLayoutRelationEqual toItem:aView.superview attribute:centerAttr multiplier:1.0 constant:0.];
     
     [aView.superview addConstraint:centerLayout];
     
-    [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:aView.superview attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.]];
+    [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:posAttr relatedBy:NSLayoutRelationEqual toItem:aView.superview attribute:posAttr multiplier:1.0 constant:0.]];
     
     [aView.superview addConstraint:[NSLayoutConstraint constraintWithItem:aView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:aView.superview attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0.]];
     
---
+++ BMExtendablePageController/Transitions/BMVerticalFlipTransition.h
@@ -0,0 +1,13 @@
+//
+//  BMHorizontalFlipTransition.h
+//  BMExtendablePageController
+//
+//  Created by mno on 13.05.13.
+//  Copyright (c) 2013 urbn;. All rights reserved.
+//
+
+#import "BMExtendablePageController.h"
+
+@interface BMVerticalFlipTransition : NSObject <BMExtendablePageTransition>
+
+@end
